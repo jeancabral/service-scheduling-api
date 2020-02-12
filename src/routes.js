791c8ac1d0e76b61d1ca82c3de5f1681c65f0a70
Jean Cabral <jeancabral@gmail.com>
2020-02-12 00:28:15 -0200

    nodemon, sucrase e degug config

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 
 routes.get('/', (req, res) => {
-  return res.json({ message: "Olá Mundão..."})
+  return res.json({ message: "Olá Mundãooo..."});
 })
 
-module.exports = routes;
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const app = require('./app');
+import app from './app';
 
 app.listen(3333, () => {
   console.log(`🤖  Hello! The app is listening on port 3333!`);
