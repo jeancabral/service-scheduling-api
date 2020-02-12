04b888c4d0b42a32384fe486a71367cacbd2442f
Jean Cabral <jeancabral@gmail.com>
2020-02-12 11:48:53 -0200

    loader de models

---
+++ src/database/index.js
@@ -0,0 +1,31 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    // Testing the connection
+    this.connection
+      .authenticate()
+      .then(() => {
+        console.log('Connection has been established successfully.');
+      })
+      .catch(err => {
+        console.error('Unable to connect to the database:', err);
+      });
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
